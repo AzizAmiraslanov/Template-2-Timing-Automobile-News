efb9aa64e09acb3f5a3d3d9fc40f375956f38848
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2021-02-12 22:55:48 +0400

    Automobile News

---
+++ 18.11.2020 gece/js/app.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+document.getElementById("read-more").addEventListener("click", function() {
+  preventDefault();
+});
